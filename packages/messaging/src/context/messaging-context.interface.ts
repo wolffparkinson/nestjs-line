68cc50b9aa08f06ae3ad525f74d8080a39b867ac
wolffparkinson <krishanu1296@gmail.com>
2022-10-11 07:53:20 +0530

    types(messaging): add PostbackCtx type

--- packages/messaging/src/context/messaging-context.interface.ts
+++ packages/messaging/src/context/messaging-context.interface.ts
@@ -1,7 +1,10 @@
-import { MessageEvent, TextEventMessage } from '@line/bot-sdk';
+import { MessageEvent, PostbackEvent, TextEventMessage } from '@line/bot-sdk';
 import { MessagingClientEvents } from '../listeners/listener.interface';
 
 export type TextCommandContext = [MessageEvent & { message: TextEventMessage }];
 export type TextCommandCtx = TextCommandContext;
+
+export type PostbackContext = [PostbackEvent];
+export type PostbackCtx = PostbackContext;
 
 export type ContextOf<K extends keyof E, E = MessagingClientEvents> = E[K];
