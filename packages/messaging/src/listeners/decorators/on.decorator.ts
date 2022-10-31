16ca7f0c3319275795cad60b184b0251aa8fa5f8
wolffparkinson <krishanu1296@gmail.com>
2022-10-31 16:48:37 +0530

    fix(messaging): return type of On decorator

--- packages/messaging/src/listeners/decorators/on.decorator.ts
+++ packages/messaging/src/listeners/decorators/on.decorator.ts
@@ -2,5 +2,5 @@ import { Listener } from './listener.decorator';
 import { MessagingClientEvents } from '../listener.interface';
 
 export const On = <K extends keyof MessagingClientEvents>(...events: K[]) => {
-  Listener({ events });
+  return Listener({ events });
 };
